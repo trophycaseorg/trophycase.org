128c8aa2984d3653a17885c072e7df58fbc567fd
LimeGradient <fnafgamer1yt@gmail.com>
2026-04-28 12:31:37 -0400

    feat: data exporting

--- app/components/game_dialog.tsx
+++ app/components/game_dialog.tsx
@@ -4,12 +4,19 @@ import { PSNGame, PSNTrophy, trophyRarity } from "@/lib/types/psn";
 import TrophyCell from "./trophy_cell";
 import { Combobox, ComboboxContent, ComboboxEmpty, ComboboxInput, ComboboxItem, ComboboxList } from "@/components/ui/combobox";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import * as converter from "json-2-csv"
 
 interface GameDialogProps {
     open: boolean,
     handleClose: () => void,
     trigger: React.ReactNode,
     game: PSNGame
+}
+
+enum DataExportType {
+    JSON = 0,
+    CSV = 1
 }
 
 export default function GameDialog(props: GameDialogProps) {
@@ -41,6 +48,43 @@ export default function GameDialog(props: GameDialogProps) {
         setTrophies(t)
     }
 
+    const exportingMethods = ["JSON", "CSV"]
+    const [exportMethod, setExportMethod] = useState<DataExportType>()
+    const exportGameData = async () => {
+        switch (exportMethod) {
+            case DataExportType.JSON: {
+                const dataString = JSON.stringify(game, null, 4)
+                const blob = new Blob([dataString], {type: "application/json"})
+
+                const url = URL.createObjectURL(blob)
+                const link = document.createElement("a")
+                link.href = url
+                link.download = `${game.name}_data.json`
+                document.body.appendChild(link)
+                link.click()
+
+                document.body.removeChild(link)
+                URL.revokeObjectURL(url)
+
+                break
+            }
+            case DataExportType.CSV: {
+                const csv = await converter.json2csv([game])
+                const blob = new Blob([csv], {type: "text/plain"})
+
+                const url = URL.createObjectURL(blob)
+                const link = document.createElement("a")
+                link.href = url
+                link.download = `${game.name}_data.csv`
+                document.body.appendChild(link)
+                link.click()
+
+                document.body.removeChild(link)
+                URL.revokeObjectURL(url)
+            }
+        }
+    }
+
     return (
         <Dialog open={open}>
             {trigger}
@@ -48,19 +92,35 @@ export default function GameDialog(props: GameDialogProps) {
                 <DialogHeader className="flex justify-self-center items-center">
                     <DialogTitle className="text-2xl">{game.name}</DialogTitle>
                     <br />
-                    <Combobox items={sortingMethods} onValueChange={(val) => sortTrophies(val as string)}>
-                        <ComboboxInput placeholder="Sort by" />
-                        <ComboboxContent>
-                            <ComboboxEmpty>No sort found.</ComboboxEmpty>
-                            <ComboboxList>
-                                {(sort) => (
-                                    <ComboboxItem key={sort} value={sort}>
-                                        {sort}
-                                    </ComboboxItem>
-                                )}
-                            </ComboboxList>
-                        </ComboboxContent>
-                    </Combobox>
+                    <div className="flex flex-row gap-4">
+                        <Combobox items={sortingMethods} onValueChange={(val) => sortTrophies(val as string)}>
+                            <ComboboxInput placeholder="Sort by" />
+                            <ComboboxContent>
+                                <ComboboxEmpty>No sort found.</ComboboxEmpty>
+                                <ComboboxList>
+                                    {(sort) => (
+                                        <ComboboxItem key={sort} value={sort} className="pointer-events-auto">
+                                            {sort}
+                                        </ComboboxItem>
+                                    )}
+                                </ComboboxList>
+                            </ComboboxContent>
+                        </Combobox>
+                        <Combobox items={exportingMethods} onValueChange={(val) => setExportMethod(val as DataExportType)}>
+                            <ComboboxInput placeholder="Export as" value={exportingMethods.at(exportMethod as number)} />
+                            <ComboboxContent>
+                                <ComboboxEmpty>No method found.</ComboboxEmpty>
+                                <ComboboxList>
+                                    {(exportType) => (
+                                        <ComboboxItem key={exportType} value={exportingMethods.indexOf(exportType) as DataExportType} className="pointer-events-auto">
+                                            {exportType}
+                                        </ComboboxItem>
+                                    )}
+                                </ComboboxList>
+                            </ComboboxContent>
+                        </Combobox>
+                        <Button className="cursor-pointer" onClick={exportGameData}>Export</Button>
+                    </div>
                 </DialogHeader>
                 <Separator orientation="horizontal" />
                 <div className="flex flex-col gap-4 justify-self-center w-300 h-200 overflow-y-scroll no-scrollbar border-4 p-4">
